1cd3f975f40df781ffd69029e1e231ce9778b474
Su-Yeon Bak <bsuyeon16@gmail.com>
2019-11-14 14:27:48 +0900

    get weather from openweatherapi using axios

--- App.js
+++ App.js
@@ -4,11 +4,19 @@ import { Alert } from 'react-native';
 import Loading from './Loading';
 
 import * as Location from 'expo-location';
+import axios from 'axios';
+
+const API_KEY = '4de275e9c1dc478d273652025b925b02';
 
 export default class extends React.Component {
   state = {
     isLoading: true
   };
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`);
+    console.log(data);
+  }
 
   getLocation = async () => {
     try {
@@ -16,6 +24,7 @@ export default class extends React.Component {
       const {
         coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("Can't find your location.", "Please allow location permission");
