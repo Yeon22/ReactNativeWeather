bbbedd2a93ef8960ecfda3b0f54fa56637fb49c3
Su-Yeon Bak <bsuyeon16@gmail.com>
2019-11-14 19:11:21 +0900

    add icon on Weather

--- App.js
+++ App.js
@@ -15,8 +15,17 @@ export default class extends React.Component {
   };
 
   getWeather = async (latitude, longitude) => {
-    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`);
-    this.setState({ isLoading: false, temp: data.main.temp });
+    const {
+      data: {
+        main: { temp },
+        weather,
+      }
+    } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`);
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp,
+    });
   }
 
   getLocation = async () => {
@@ -36,7 +45,7 @@ export default class extends React.Component {
   }
 
   render() {
-    const { isLoading, temp } = this.state;
-    return isLoading ? <Loading/> : <Weather temp={Math.round(temp)}/>;
+    const { isLoading, condition, temp } = this.state;
+    return isLoading ? <Loading/> : <Weather condition={condition} temp={Math.round(temp)}/>;
   }
 }
--- Weather.js
+++ Weather.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
+import { Ionicons, Feather, MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Weather({temp}) {
   return (
     <View style={styles.container}>
-      <Text>{temp}</Text>
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons name="weather-fog" size={90}/>
+        <Text style={styles.temp}>{temp}º</Text>
+      </View>
+      <View style={styles.halfContainer}>
+
+      </View>
     </View>
   );
 }
 
 Weather.propTypes = {
-  temp: PropTypes.number.isRequired
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Mist",
+    "Smoke",
+    "Haze",
+    "Dust",
+    "Fog",
+    "Sand",
+    "Ash",
+    "Squall",
+    "Tornado",
+    "Clear",
+    "Clouds"
+  ]).isRequired
 };
 
 const styles = StyleSheet.create({
@@ -19,5 +43,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  temp: {
+    fontSize: 36
   }
 });
