f20dee84b27aa0745f588833af4d106dbac9af90
Su-Yeon Bak <bsuyeon16@gmail.com>
2019-11-30 22:05:44 +0900

    Replace Text, View to styled-components

--- components/Loading.js
+++ components/Loading.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
+import styled from 'styled-components';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+  align-items: center;
+  padding-horizontal: 30;
+  padding-vertical: 100;
+  background-color: #FDF6AA;
+`;
+
+const LoadingText = styled.Text`
+  color: #2c2c2c;
+  font-size: 30;
+`;
 
 export default function Loading() {
   return (
-    <View style={styles.container}>
+    <Container>
       <StatusBar barStyle="dark-content"/>
-      <Text style={styles.text}>Getting the weather</Text>
-    </View>
+      <LoadingText>Getting the weather</LoadingText>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
-    paddingHorizontal: 30,
-    paddingVertical: 100,
-    backgroundColor: "#FDF6AA"
-  },
-  text: {
-    color: "#2c2c2c",
-    fontSize: 30
-  }
-});
--- components/Weather.js
+++ components/Weather.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import { LinearGradient } from 'expo-linear-gradient';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { WEATHERS, WEATHER_OPTIONS, ICON_SIZE, ICON_COLOR } from '../constant';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TempText = styled.Text`
+  font-size: 36;
+  color: white;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 45;
+  font-weight: 300;
+  margin-bottom: 10;
+`;
+
+const SubTitle = styled.Text`
+  font-weight: 600;
+  color: white;
+  font-size: 24;
+`;
 
 export default function Weather({temp, condition, description}) {
   return (
@@ -13,17 +38,17 @@ export default function Weather({temp, condition, description}) {
         colors={WEATHER_OPTIONS[condition].gradient}
         style={styles.container}>
       <StatusBar barStyle="light-content"/>
-      <View style={styles.halfContainer}>
+      <Container>
         <MaterialCommunityIcons
             name={WEATHER_OPTIONS[condition].iconName}
             size={ICON_SIZE}
             color={ICON_COLOR}/>
-        <Text style={styles.temp}>{temp}º</Text>
-      </View>
-      <View style={styles.halfContainer}>
-        <Text style={styles.title}>{condition}</Text>
-        <Text style={styles.subtitle}>{description}</Text>
-      </View>
+        <TempText>{temp}º</TempText>
+      </Container>
+      <Container>
+        <Title>{condition}</Title>
+        <SubTitle>{description}</SubTitle>
+      </Container>
     </LinearGradient>
   );
 }
@@ -38,25 +63,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
-  },
-  halfContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  temp: {
-    fontSize: 36,
-    color: "white"
-  },
-  title: {
-    color: "white",
-    fontSize: 45,
-    fontWeight: "300",
-    marginBottom: 10
-  },
-  subtitle: {
-    fontWeight: "600",
-    color: "white",
-    fontSize: 24
   }
 });
